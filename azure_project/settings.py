bdd767d8416b568d7d2a7da08ded9e8adef36a53
unknown <supavit.sutha@mail.kmutt.ac.th>
2026-02-05 14:59:33 +0700

    initial commit

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -89,14 +89,14 @@ WSGI_APPLICATION = 'azure_project.wsgi.application'
 # }
 DATABASES = {
     'default': {
-        'ENGINE': 'mssql',
-        'NAME': 'bearlab-sql-db',
-        'USER': 'bearlab-admin',
-        'PASSWORD': 'P@ss1234',
-        'HOST': 'bearlab-sql-server.database.windows.net',
-        'PORT': '1433',
-        'OPTIONS': {
-            'driver': 'ODBC Driver 18 for SQL Server',
+         'ENGINE': 'mssql',
+         'NAME': 'inc372-test-m',
+         'USER': 'supavit.sutha',
+         'PASSWORD': 'Oishilemon_11',
+         'HOST': 'inc372-test-m.database.windows.net',
+         'PORT': '1433',
+         'OPTIONS': {
+             'driver': 'ODBC Driver 18 for SQL Server',
         },
     },
 }
